8e9c6539fbb1565d9629c907f9c226ac1770cff0
Sávio Almeida <savio.pinheiroal@gmail.com>
2023-07-03 16:02:02 -0300

    add globalStyle

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -2,8 +2,24 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 
+html {
+   font-size: 10px;
    body {
-      /* background-color: red; */
-   }
+      overflow-x: hidden;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      background-color: red;
 
-`;
+      a {
+         color: inherit;
+         text-decoration: none;
+      }
+      li {
+         list-style-type: none;
+      }
+      ul {
+         padding: 0;
+      }
+   }
+}`;
