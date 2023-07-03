8f140d194176c0e05b315e051975402656781781
Sávio Almeida <savio.pinheiroal@gmail.com>
2023-07-03 20:13:35 -0300

    add more styles at globalStyle

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -9,17 +9,29 @@ html {
       margin: 0;
       padding: 0;
       box-sizing: border-box;
-      background-color: red;
 
-      a {
+      div {
+         margin: 0;
+         padding: 0;
+      }
+      a, p {
          color: inherit;
          text-decoration: none;
+         margin: 0;
+         padding: 0;
       }
       li {
          list-style-type: none;
       }
       ul {
          padding: 0;
+      }
+
+      main {
+         height: 100vh;
+         display: flex;
+         align-self: center
+         ;
       }
    }
 }`;
